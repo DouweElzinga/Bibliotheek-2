ba4e93cb692095c033bd8a95a27278854a93f232
DESKTOP-CVDSM7O\Douwe <douweelzinga98@gmail.com>
2021-02-11 15:42:22 +0100

    Learning view cells and working with libraries.

--- app/Views/blog.php
+++ app/Views/blog.php
@@ -1,15 +1,7 @@
 <h1><?= $title ?></h1>
 
-
-
-
-<div>
+<div class="row">
     <?php foreach($posts as $post) : ?> 
-
-        <div>
-            <h3><?= $post ?></h3>
-            <img src="assets/images/Avatar icon.png" style="width: 100px; height: auto;">
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus odio a quae expedita quasi quibusdam consequatur consectetur. A, nam dolorem sunt accusamus sapiente exercitationem repudiandae illo, facere, quae commodi repellendus!</p>
-        </div>
+        <?= view_cell('\App\Libraries\Blog::postItem', ['title' => $post])?>
     <?php endforeach; ?>
 </div>
---
+++ app/Views/components/post_item.php
@@ -0,0 +1,12 @@
+
+
+<div class="card" style="width: 18rem;">
+  <img src="assets/images/Avatar icon.png" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title"><?= $title ?></h5>
+    <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus odio a quae expedita quasi quibusdam consequatur consectetur. A, nam dolorem sunt accusamus sapiente exercitationem repudiandae illo, facere, quae commodi repellendus!</p>
+    <a href="/blog/post" class="btn btn-primary">Read more</a>
+  </div>
+</div>
+
+
