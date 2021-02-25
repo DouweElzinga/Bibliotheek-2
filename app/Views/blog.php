5b44e345b009196d1598f80d43e1ede8398ea124
Jan Zuur <J.Zuur@fcroc.nl>
2021-02-25 16:30:43 +0100

    Revert "Mijn opmerkingen"
    This reverts commit a28e7f8dbeb5d1c8b8245be84e674e417396123a.

--- app/Views/blog.php
+++ app/Views/blog.php
@@ -6,7 +6,6 @@
 
 <div class="row">
     <?= $this->include('widgets/sidebar'); ?>
-    <!-- zuja: Hier ergen een add aan toevoegen -->
     <div class="col-12 col-sm-9">
         <div class="row">
             <?php
@@ -15,18 +14,15 @@
                 $results = $query->getResult();
             
 
-                foreach ($results as $row) {
-            ?>
-                    <div class='card col-12 col-sm-6 col-md-4' style='width: 200px;'>
-                        <img src='assets/images/Avatar icon.png' class='card-img-top'>
-                        <div class='card-body'>
-                                <h5 class='card-title'><?= $row->post_title ?></h5>
-                                <p class='card-text' style='font-size: 12px'><?= $row->post_content ?><p>
-                                <a href='/blog/post/<?= $row->post_id ?>' class='btn btn-primary'>Read more</a>
-                                <!-- zuja: Hier een delete en edit aan toevoegen -->
-                        </div>
-                    </div>
-            <?php
+                 foreach ($results as $row) {
+                    echo "<div class='card col-12 col-sm-6 col-md-4' style='width: 200px;'>";
+                        echo "<img src='assets/images/Avatar icon.png' class='card-img-top'>";
+                        echo "<div class='card-body'>";
+                                echo "<h5 class='card-title'>" . $row->post_title . "</h5>";
+                                echo "<p class='card-text' style='font-size: 12px'>" . $row->post_content . "<p>";
+                                echo "<a href='/blog/post/" . $row->post_id . "' class='btn btn-primary'>Read more</a>";
+                        echo "</div>";
+                    echo "</div>";
                 }
             ?>          
         </div>
--- app/Views/single_post.php
+++ app/Views/single_post.php
@@ -1,12 +1,11 @@
 <?= $this->extend('layouts/main'); ?>
 
 <?= $this->section('content') ?>
-    <h1><?= (isset($post))?$post['post_title']:'no record set'; ?></h1>
-    <?php if(isset($post)): ?>
-        <p><?= $post['post_content'] ?></p>
+    <h1><?= $title ?></h1>
+    <p><?= $content ?></p>
 
-        <a href="/blog/delete/<?= $post['post_id'] ?>" class="btn btn-danger">Delete</a>
-        <a href="/blog/edit/<?= $post['post_id'] ?>" class="btn btn-primary">edit</a>
-    <?php endif; ?>
+<a href="/blog/delete/<?= $post['post_id'] ?>" class="btn btn-danger">Delete</a>
+<a href="/blog/edit/<?= $post['post_id'] ?>" class="btn btn-primary">edit</a>
+
 
 <?= $this->endSection() ?>
